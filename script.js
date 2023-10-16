d21f881f457bfb6d89b9052e779c99a80a7d0d85
Eecodev <mnasser.bbk@gmail.com>
2023-10-16 18:03:06 +0200

    shoppinglist items prompted and stored using while

--- script.js
+++ script.js
@@ -7,3 +7,27 @@
 * Data la lista della spesa creata in precedenza, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
 */
 
+
+let list = [];
+let item = '';
+
+while(item !== 'stop'){
+    item = prompt('Aggiungi alla lista');
+
+    if( item !== 'stop' && item !== ''){
+        list.push(item);
+    }
+
+    console.log(item);
+}
+
+console.log(list);
+
+const listEl = document.getElementById('list');
+const listItemEl = document.createElement('li');
+
+for ( i = 0; i < list.length; i++) {
+    const listItemEl = document.createElement('li');
+    listItemEl.innerHTML = list[i];
+    listEl.appendChild(listItemEl);
+}
